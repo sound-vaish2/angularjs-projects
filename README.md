4805368a25f050783934a1b8ddde4753a2df5915
sound-vaish2 <vaishnavisoundane@gmail.com>
2020-06-09 16:10:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angularjs-projects
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angularjs-projects)
---
+++ angular-projects/app.js
@@ -0,0 +1,9 @@
+(function(){
+'use strict';
+
+
+angular.module('name calculator', [])
+.controller('first controller', function($scope)
+  
+});
+})();
